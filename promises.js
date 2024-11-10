6454d5306b852c1e47957d38202a4175a2fbb62e
demirose41 <demirose41@gmail.com>
2024-11-09 17:36:24 -0800

    completed practice

--- promises.js
+++ promises.js
@@ -71,17 +71,48 @@ waiting()
 /* -------------------- exploring reject and .catch -------------------- */
 
 // Your code here
+const tryRandomPromise = (random) => new Promise((resolve, reject) => {
+    if (random > 0.5){
+        resolve('success!!!!');
+    }else{
+        reject('random error!')
+    }
+});
 
+// for (let i = 1; i < 10; i++){
+//     const random = Math.random();
+//     wait(2000 + random * 1000)
+//         .then(() => tryRandomPromise(random))
+//         .then( result => console.log(` random try #${i} : `, result))
+//         .catch( error => console.error(`random try #${i} : `, error))
+// }
 
 
 /* ============================== Phase 7 ============================== */
 /* ---------------- exploring async/await and try/catch ---------------- */
 
 // Your code here
+const tryTryAgain = async (i) => {
+    const random = Math.random();
 
+    await wait(3000 + random * 1000);
+
+    try {
+        const result = await tryRandomPromise(random);
+        console.log('random again #', i, result);
+    } catch (error) {
+        console.error('random again #', i, error)
+    }
+}
+
+for (let i = 1; i < 10; i++){
+    tryTryAgain(i);
+}
 
 
 /* ============================== Phase 8 ============================== */
 /* -------------------- Promises are asynchronous! --------------------- */
 
 // Your code here
+
+console.log('END OF PROGRAM')
