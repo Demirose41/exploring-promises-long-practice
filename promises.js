04cd83dd0f3e772aa5a2a1dc990aefa3f1d91f4d
demirose41 <demirose41@gmail.com>
2024-11-09 17:17:17 -0800

    1:completed phases 1-5

--- promises.js
+++ promises.js
@@ -1,7 +1,16 @@
 /* ============================== Phase 1 ============================== */
 /* -------------------------- exploring async -------------------------- */
 
-// Your code here
+function num1(){
+    return 1;
+}
+
+async function num2(){
+    return 2;
+}
+
+console.log('num1', num1());
+async () => { console.log('num2', await num2())}
 
 
 
@@ -10,6 +19,11 @@
 
 // Your code here
 
+async function waiting() {
+    const value = await num2();
+    console.log('Waiting', await num2());
+}
+// waiting();
 
 
 /* ============================== Phase 3 ============================== */
@@ -17,20 +31,40 @@
 
 // Your code here
 
+async function waitForMyPromise() {
+    const promise = new Promise ((resolve) => {
+        setTimeout(() => {
+            resolve('done!!')
+        }, 1000)
+    });
+
+    const result = await promise
+    console.log("my promise is", result)
+}
+// waitForMyPromise()
 
 
 /* ============================== Phase 4 ============================== */
 /* -------------------------- exploring then --------------------------- */
 
 // Your code here
-
+new Promise ((resolve) => {
+    setTimeout(() => {
+        resolve('done!');
+    }, 1500)
+}).then( r => console.log('my promise is', r))
 
 
 /* ============================== Phase 5 ============================== */
 /* ------------------- turn setTimeout into a Promise ------------------ */
 
 // Your code here
-
+function wait(ms) {
+    return new Promise( resolve  => setTimeout(resolve, ms));
+}
+waiting()
+.then(() => wait(1500))
+.then(() => waiting())
 
 
 /* ============================== Phase 6 ============================== */
